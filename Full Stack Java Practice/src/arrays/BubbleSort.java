64f8176adabfba3da7e693e9e1bc0708004d04df
mdakram09 <iamakramkhan09@gmail.com>
2022-07-07 16:50:01 +0530

    practiced bubble sort

--- Full Stack Java Practice/src/arrays/BubbleSort.java
+++ Full Stack Java Practice/src/arrays/BubbleSort.java
@@ -1,4 +1,33 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class BubbleSort {
+    public static void bubbleSort(int[] arr){
+        int n= arr.length;
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-1-i; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("Sorted Array: ");
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the size of the Array: ");
+        int n= s.nextInt();
+        System.out.println("Enter "+n+" elements: ");
+        int[] arr= new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=s.nextInt();
+        }
+        bubbleSort(arr);
+    }
 }
